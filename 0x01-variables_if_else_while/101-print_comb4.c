8abea7dfe0b85855c189d9fc066e00130618f364
TimmyN-eng <timmyneutron097@gmail.com>
2022-09-12 20:05:35 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	int c, i, k;
 
-	for (c = "0"; c <= '9' ; c++)
+	for (c = '0'; c <= '9' ; c++)
 	{
-		for (i + '0'; i <= '9'; i++)
+		for (i = '0'; i <= '9'; i++)
 		{
 			for (k = '0'; k <= '9'; k++)
 			{
@@ -30,11 +30,12 @@ int main(void)
 					putchar(',');
 
 					putchar(' ');
-					]
-						]
-						]
-						]
-						]
+				}
+				}
+			}
+		}
+	}
+	
 						putchar('\n');
 					return (0);
 				}
