5d44a7c2ec8b6c929cb61a0d8624dfcfa130d7cf
TimmyN-eng <timmyneutron097@gmail.com>
2022-09-14 13:53:51 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,22 +7,19 @@
  */
 
 void jack_bauer(void)
+{
+	int min, hr;
 
+	for (hr = 0; hr <= 23; ++hr)
+	{
+		for (min = 0; min <= 59; ++min)
 		{
-		int a;
-		int b;
-
-		for (a = 0; a <= 23; a++)
-		{
-		for (b = 0; b <= 59; b++)
-		{
-		_putchar (a / 10 + '0');
-		_putchar (a % 10 + '0');
-		_putchar (':');
-		_putchar (b / 10 + '0');
-		_putchar ( '\n');
+			_putchar((hr / 10) + 48);
+			_putchar((hr % 10) + 48);
+			_putchar(':');
+			_putchar((min / 10) + 48);
+			_putchar((min % 10) + 48);
+			_putchar('\n');
 		}
-
-		}
-
-		}
+	}
+}
