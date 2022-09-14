58493934e47c24588ee38e62961d3be6bf91e5de
TimmyN-eng <timmyneutron097@gmail.com>
2022-09-14 10:58:58 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,49 +1,39 @@
 #include "main.h"
 /**
  * print_times_table - prints times table of n
- * @n : times table to use
- * Return: void
- */
+ *@n: takes number input
+*/
 
-void print_times_table(int n)
-{
-	int a = 0, rep, b;
+ void print_times_table(int n)
+ {
+ int prod, mult, num;
 
-	if (n < 0 || n > 15)
-		return;
+ if (n <= 15 && n >= 0)
+ {
+	 for (num = 0; num <= n; ++num)
+	 {
+		 _putchar(48);
+		 for (mult = 1; mult <= n; ++mult)
+		 {
+			 _putchar(',');
+			 _putchar(' ');
 
-	while (a <= n)
-	{
-		for (b = 0; b <= n; b++)
-		{
-			rep = a * b;
-			if (b == 0)
-				_putchar('0' + rep);
-			else if (rep < 10)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + rep);
-			}
-			else if (rep < 100)
-			{
-				_putchar(' ');
-				_putchar('0' + rep / 10);
-				_putchar('0' + rep % 10);
-			}
-			else
-			{
-				_putchar('0' + rep / 100);
-				_putchar('0' + (rep -100) / 10);
-				_putchar('0' + rep % 10);
-			}
-			if (b < n)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
-		_putchar('\n');
-		a++;
-	}
-}
+			 prod = num * mult;
+
+			 if (prod <= 9)
+				 _putchar(' ');
+			 if (prod <= 99)
+				 _putchar(' ');
+
+			 if (prod >= 100)
+			 {
+				 _putchar((prod / 100) + 48);
+				 _putchar((prod / 10) % 10 + 48);
+			 } else if (prod <= 99 && prod >= 10)
+				 _putchar((prod / 10) + 48);
+			 _putchar((prod % 10) + 48);
+		 }
+		 _putchar('\n');
+	 }
+ }
+ }
