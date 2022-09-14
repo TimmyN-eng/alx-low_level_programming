f56caeb5f818edabae389c2fe9b48c7373871667
TimmyN-eng <timmyneutron097@gmail.com>
2022-09-14 09:22:22 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -26,14 +26,11 @@ void times_table(void)
 				{
 
 					_putchar(' ');
-
 					_putchar((c % 10) + '0');
 				}
 				if (b < 9)
 				{
-
 					_putchar(',');
-
 					_putchar(' ');
 				}
 			}
@@ -43,10 +40,8 @@ void times_table(void)
 				_putchar((c % 10) + '0');
 				if (b < 9)
 				{
-
-					_putchar(',');
-
-					_putchar(' ');
+			_putchar(',');
+			_putchar(' ');
 				}
 			}
 		}
