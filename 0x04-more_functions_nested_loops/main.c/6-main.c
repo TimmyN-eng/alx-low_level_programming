1609cdd205af3d7b0c61a2f88245550d4560c83a
TimmyN-eng <timmyneutron097@gmail.com>
2022-09-15 07:50:37 -0700

    main.c

---
+++ 0x04-more_functions_nested_loops/main.c/6-main.c
@@ -0,0 +1,16 @@
+nclude "../main.h"
+
+/**
+ * main - check the code for ALX School students
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	print_line(0);
+	print_line(2);
+	print_line(10);
+	print_line(-4);
+	return (0);
+}
