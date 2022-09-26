dc8579ee9673e79a5c52116d923c9964fb363114
TimmyN-eng <timmyneutron097@gmail.com>
2022-09-26 09:25:06 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -16,14 +16,14 @@ char *_strstr(char *haystack, char *needle)
 		str1 = haystack;
 		str2 = needle;
 
-		while (*haystack != '\0' && *str2 != '\0' && haystack == *str2)
+		while (*haystack != '\0' && *str2 != '\0' && *haystack == *str2)
 		{
 			haystack++;
 			str2;
 		}
 		if (*str2 == '\0')
 			return (str1);
-		haystack = str1 = 1;
+		haystack = str1 + 1;
 	}
 	return (0);
 }
